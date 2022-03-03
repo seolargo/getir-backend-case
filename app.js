277bec097a7921c6aea633bdf332dce6cd5663c3
ömer <hngr1234>
2022-03-04 02:37:05 +0300

    second push

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post('/filterRecords', function(req, res) {
     }
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     //console.log('3000 works!')
 })
 
