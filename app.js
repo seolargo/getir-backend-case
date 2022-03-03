0aa93624cb463c8197fa75ebb0e549fd32ee75fc
ömer <hngr1234>
2022-03-04 02:34:31 +0300

    second push

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
 const dbURI = 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true';
 
+app.get('/', function(req, res) {
+    res.status(200).send('Hello world');
+})
+
 app.post('/filterRecords', function(req, res) {
     const startDate = req.body.startDate || '2014-01-01T00:00:00.921Z';
     const endDate = req.body.endDate || '2016-12-31T00:00:00.921Z';
